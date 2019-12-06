6f15274612668a60f0a7dae8f3526b1383a491b5
Pauline Liang <paulineliang7829@gmail.com>
2019-12-06 18:42:55 -0500

    edits

--- Projects/group3/docs/conf.py
+++ Projects/group3/docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'somename'
+project = 'THEpackage'
 copyright = '2019, Group 3'
 author = 'Group 3'
 
