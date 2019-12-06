4ae40503b1423db47ea607372b5523c2f272123c
Jett Pettus <jettpettus@gmail.com>
2019-12-06 16:55:32 -0500

    Ed has horrible documentation

--- Projects/project_2_packages/iv_jett/iv_standard_error.py
+++ Projects/project_2_packages/iv_jett/iv_standard_error.py
@@ -1,12 +1,14 @@
+'''
+Calculate Covariance Matrix
+'''
+
 import numpy as np
 from iv_jett.iv_init import projection_matrix
 
 def calculate_sigma(Z, X, Y, beta_iv):
     N = Z.shape[0]
     resid = Y - X @ beta_iv
-
     sigma_iv = (1/N) * np.transpose(resid) @ resid
-
     return np.sqrt(sigma_iv)
 
 def calculate_var_beta(sigma, X, Z):
