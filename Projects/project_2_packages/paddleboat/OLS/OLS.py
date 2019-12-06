6b54a09e5502e18b565e687e29d0858108115e32
joel-becker <joel_becker@mail.com>
2019-12-06 15:10:54 -0500

    Space functions better

--- Projects/project_2_packages/paddleboat/OLS/OLS.py
+++ Projects/project_2_packages/paddleboat/OLS/OLS.py
@@ -3,29 +3,38 @@ import numpy as np
 
 def get_betas(X, Y):
     betas = (transpose(X) * X)^(-1) * (transpose(X) * Y)
+    
     print("Working!")
+
     return betas
 
 def get_residuals(betas, X):
     residuals = betas * X
+
     print("Working!")
+
     return residuals
 
 def get_n(X, Y):
     n_X = length(X)
     n_Y = length(Y)
+
     if n_X == n_Y:
         n = n_X
     else:
         print("Error!")
+
     print("Working!")
+
     return n
 
 def get_ses():
     residuals2 = residuals^2
     XX = (transpose(X) * X)^(-1)
     ses = (residuals2 / (N-1)) * XX
+
     print("Working!")
+
     return ses
 
 def main():
