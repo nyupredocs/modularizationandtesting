ecd2e7bff9350e804312896ad77125b223713f7d
joel-becker <joel_becker@mail.com>
2019-12-06 14:59:36 -0500

    get_betas pseudocode

--- Projects/project_2_packages/paddleboat/OLS/OLS.py
+++ Projects/project_2_packages/paddleboat/OLS/OLS.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 
-def get_betas():
+def get_betas(X, Y):
+    betas = (transpose(X) * X)^(-1) * (transpose(X) * Y)
+    return betas
     print("Working!")
 
 def get_ses():
