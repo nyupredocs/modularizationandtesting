0c1fe8cd677f98d1c1ee476020baef5baa10df8c
joel-becker <joel_becker@mail.com>
2019-12-06 15:54:34 -0500

    Add LASSO.py

---
+++ Projects/project_2_packages/paddleboat/LASSO/LASSO.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+
+def get_betas(X, Y):
+    """Get betas (according to OLS formula)"""
+    betas = (transpose(X) * X)^(-1) * (transpose(X) * Y) # transpose is not a real function
+
+    print("Working!")
+    return betas
+
+def get_residuals(betas, X, Y):
+    """Get residuals (according to OLS formula)"""
+    y_hat = betas * X
+    residuals = Y - y_hat
+
+    print("Working!")
+    return residuals
+
+def get_n(X, Y):
+    """Get N, check independent vs dependent variables"""
+    n_X = length(X)
+    n_Y = length(Y)
+
+    if n_X == n_Y:
+        n = n_X
+    else:
+        print("Error!")
+
+    print("Working!")
+    return n
+
+def get_ses(residuals, X, Y):
+    """Get SEs (according to OLS formula)"""
+    residuals2 = residuals^2
+    XX = (transpose(X) * X)^(-1) # transpose is not a real function
+    N = get_n(X, Y)
+    ses = (residuals2 / (N-1)) * XX
+
+    print("Working!")
+    return ses
+
+def get_r2(Y, X, betas):
+    """Get R^2"""
+    y_hat = X * betas
+    y_bar = mean(y)
+
+    SSR = sum((y_hat - y_bar)^2)
+    SST = sum((y - y_bar)^2)
+
+    r2 = SSR / SST
+
+    print("Working!")
+    return r2
+
+def main():
+    """Performs OLS, prints output to table"""
+    print("Working!")
