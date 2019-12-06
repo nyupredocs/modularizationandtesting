9ad9d18d76803b1bc67a5982400053207dd7d522
Franco Calle <francocalle93@gmail.com>
2019-12-06 16:54:36 -0500

    update

--- Projects/project_2_packages/wethebestOLS/wethebestOLS/logit/logit.py
+++ Projects/project_2_packages/wethebestOLS/wethebestOLS/logit/logit.py
@@ -89,6 +89,58 @@ def optimization(objectiveFunction, Y,X,bbeta):
         betaHat
 
     '''
-    betahat = minimize(lambda beta: objectiveFunction(Y,X, beta), bbeta, method='nelder-mead',options={'xtol': 1e-8, 'disp': True})
+    betahat = minimize(lambda beta: objectiveFunction(Y,X, beta), bbeta, method='nelder-mead',options={'xtol': 1e-8, 'disp': False})
 
-    return betaHat
+    return betahat
+
+
+def standardErrorsLogit(optimization, Y,X,bbeta):
+    '''
+    This is objectiveFunction.
+
+    Parameters
+    ----------
+    optimization :
+        Function that computes the parameter optimization
+    Y :
+        Array of N x 1 ones and zeros
+    X :
+        parameters of the function (K,1)
+    bbeta :
+        parameters of the function (K,1)
+
+
+    Returns
+    -------
+    sd :
+        standard deviation
+    se :
+        standard errors
+    ci :
+        confidence intervals
+
+    '''
+    nObs = X.shape[0]
+    nSE = round(nObs/10)
+    parameter_list = []
+
+
+    for i in range(1,30):
+        sample = (np.random.uniform(0,1,nObs) < 0.5)
+        Y_sample = Y[sample,:]
+        X_sample = X[sample,:]
+        betaHat = optimization(objectiveFunction, Y_sample, X_sample, bbeta)
+        parameter_list.append(betaHat['x'])
+
+    sd = np.std(np.array(parameter_list),0)
+    se = np.sqrt(sd)
+
+    betaHat = optimization(objectiveFunction, Y_sample, X_sample, bbeta)
+
+    ci = (betaHat['x'] - se, betaHat['x'] + se)
+
+    return sd, se, ci
+
+errors = standardErrorsLogit(optimization, Y,X,bbeta)
+
+sd, se, ci = errors
