6f17916df1acc8f940e98d82608d2cc31fcdaec8
Jett Pettus <jettpettus@gmail.com>
2019-12-06 15:22:00 -0500

    Added doc strings and checks

--- Projects/project_2_packages/iv_init.py
+++ Projects/project_2_packages/iv_init.py
@@ -18,18 +18,34 @@ mat_z  = np.transpose(np.matrix([[1, .5, 2, 3],
 
 mat_y  = np.transpose(np.array([[3, 5, 2, 1]]))
 
+def check_dim(a, b, c):
+    try:
+        assert a.shape[1] == b.shape[1] == c.shape[1]
+    except:
+        raise Exception('Matrices are the wrong shape')
+
+    try:
+        assert a.shape[1] <= b.shape[1]
+    except:
+        raise Exception('Model underidentified')
+
+
 def projection_matrix(b):
     '''
     Inputs:
         a (matrix) -- x
         b (matrix) -- z
     '''
+
     P_b = b @ np.linalg.inv((np.transpose(b) @ b)) @ np.transpose(b)
     return P_b
 
 def estimate_beta_iv(a, b, c):
     '''
-
+    Inputs:
+        a (matrix) -- x
+        b (matrix) -- z
+        c (matrix) -- y
     '''
 
     proj = projection_matrix(b)
@@ -37,8 +53,5 @@ def estimate_beta_iv(a, b, c):
     b_2 = np.linalg.inv(b_1)
     b_3 = np.transpose(a) @ proj @ c
     return b_2 @ b_3
-
-    #b_iv = np.linalg.inv(np.transpose(a) @ projection_matrix(b) @ a) \
-    #       @ np.transpose(a) @ projection_matrix(b) @ c
 
 print(estimate_beta_iv(mat_x, mat_z, mat_y))
