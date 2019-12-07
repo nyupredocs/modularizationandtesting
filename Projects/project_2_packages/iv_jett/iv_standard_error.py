d504c008612006dc2ddf12d86f1bac10205b0ec7
Ed Jee <edjee96@gmail.com>
2019-12-06 23:16:58 -0500

    added constant option for sigma calculations

--- Projects/project_2_packages/iv_jett/iv_standard_error.py
+++ Projects/project_2_packages/iv_jett/iv_standard_error.py
@@ -5,8 +5,14 @@ Calculate Covariance Matrix
 import numpy as np
 from iv_jett.iv_init import projection_matrix
 
-def calculate_sigma(Z, X, Y, beta_iv):
+def calculate_sigma(Z, X, Y, beta_iv, nocons = False):
+
     N = Z.shape[0]
+
+    if nocons == False:
+        X_constant = np.ones((N, 1))
+        X = np.hstack((X_constant, X))
+    
     resid = Y - X @ beta_iv
     sigma_iv = (1/N) * np.transpose(resid) @ resid
     return np.sqrt(sigma_iv)
