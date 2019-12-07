a2e3832ae6a1af5700c3aa7e9ded6f0571faf056
Ed Jee <edjee96@gmail.com>
2019-12-07 12:02:41 -0500

    fixed LOGIT YESSSS

--- Projects/project_2_packages/logit/main.py
+++ Projects/project_2_packages/logit/main.py
@@ -2,10 +2,10 @@ import numpy as np
 import math
 from scipy.optimize import minimize
 
-x = np.transpose(np.matrix([0, 1, 3, 0.5, 3, -1, 2]))
-y = np.transpose(np.matrix([9, 4, 23, 4, 4, -2, 1]))
 
 def ll_function (x, y, beta):
+    # Reshape beta
+    beta = beta[:, None]
     beta_prime = np.transpose(beta)
     fun_1 = y @ beta_prime @ x
     fun_2 = math.log(1 + math.exp(beta_prime @ x))
@@ -20,30 +20,12 @@ def fit_logit(x, y):
     res = minimize(min_func, x0 = beta_init)
     return res
 
-min_func = (lambda beta: ll_function(x, y, beta))
 
-
-minimize(min_func, beta_init[:, 0])
-beta_init = np.random.rand(len(x), 1)
-    min_func = (lambda beta: ll_function(x, y, beta))
-min_func(beta_init)
-
-
-fit_logit(x, y)
-res = minimize((lambda beta: ll_function(x, y, beta)),
-    x0 = beta0
-    )
-ll_function(x, y, beta_init)
-
-beta0 = np.random.rand(len(x), 1)
-print(beta0)
-ll_function(x, y, beta0)
-res = fmin_tnc(func = ll_function,
-               approx_grad = True,
-               x0 = 1,
-               args = (x, y))
-
-
+if __name__ == "__main__":
+    x = np.transpose(np.matrix([0, 1, 3, 0.5, 3, -1, 2]))
+    y = np.transpose(np.matrix([9, 4, 23, 4, 4, -2, 1]))
+    
+    print(fit_logit(x, y))
 
 
 
