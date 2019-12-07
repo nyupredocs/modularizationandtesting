9f8212193f6c1e0b3fbc8e04bcdae3d1636f5818
Nicolas Jimenez <njimenez@mit.edu>
2019-12-07 14:44:17 -0500

    robust SEs attempt

--- Projects/project_2_packages/iv_jett/iv_standard_error.py
+++ Projects/project_2_packages/iv_jett/iv_standard_error.py
@@ -5,6 +5,14 @@ Calculate Covariance Matrix
 import numpy as np
 from iv_jett.iv_init import projection_matrix
 
+def calculate_resid(Z, X, Y, beta_iv, nocons = False):
+    if nocons == False:
+        X_constant = np.ones((N, 1))
+        X = np.hstack((X_constant, X))
+
+    resid = Y - X @ beta_iv
+    return resid
+
 def calculate_sigma(Z, X, Y, beta_iv, nocons = False):
 
     N = Z.shape[0]
@@ -12,12 +20,13 @@ def calculate_sigma(Z, X, Y, beta_iv, nocons = False):
     if nocons == False:
         X_constant = np.ones((N, 1))
         X = np.hstack((X_constant, X))
-    
+
     resid = Y - X @ beta_iv
+
     sigma_iv = (1/N) * np.transpose(resid) @ resid
     return np.sqrt(sigma_iv)
 
-def calculate_var_beta(sigma, X, Z, nocons = False):
+def calculate_var_beta(sigma, X, Z, resid, nocons = False, robust = False):
     
     N = Z.shape[0]
 
@@ -27,6 +36,17 @@ def calculate_var_beta(sigma, X, Z, nocons = False):
         Z_constant = np.ones((N, 1))
         Z = np.hstack((Z_constant, Z))
 
-    var_beta = (np.asscalar(sigma**2) * np.identity(X.shape[1])) @ np.linalg.inv(np.transpose(X) @ projection_matrix(Z) @ X)
-    se_beta = np.sqrt(np.diag(var_beta))
+    if robust == False:
+        var_beta = (np.asscalar(sigma**2) * np.identity(X.shape[1])) @ np.linalg.inv(np.transpose(X) @ projection_matrix(Z) @ X)
+        se_beta = np.sqrt(np.diag(var_beta))
+
+    if robust == True: 
+        resid_sq = resid @ np.transpose(resid)
+        print("resid sq:")
+        print(resid_sq)
+        sandwich_bread = np.linalg.inv(np.transpose(Z) @ X)
+        var_beta = (sandwich_bread @ np.transpose(Z) @ np.diag(np.diag(resid_sq)) @ Z @ sandwich_bread)
+        print("var_beta")
+        print(var_beta)
+        se_beta = np.sqrt(np.diag(var_beta))
     return se_beta
