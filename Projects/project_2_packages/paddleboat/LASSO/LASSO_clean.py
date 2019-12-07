bd6a127057bc2a2abdaaa8ee80b88756d0a6fd29
Nadav Tadelis <nadav.tadelis@gmail.com>
2019-12-07 10:14:44 -0500

    started work on bootstrapping SEs

--- Projects/project_2_packages/paddleboat/LASSO/LASSO_clean.py
+++ Projects/project_2_packages/paddleboat/LASSO/LASSO_clean.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 import OLS as ols
 from sklearn import preprocessing
+import tqdm
 
-def get_sse(Y, X, betas):
+def sse(Y, X, betas):
         '''
         Get sum of square errors
 
@@ -23,7 +24,7 @@ def get_sse(Y, X, betas):
     return sse
 
 
-def get_loss_function(Y, X, lamb, betas):
+def loss(Y, X, lamb, betas):
         """
         Compute loss function
 
@@ -41,7 +42,7 @@ def get_loss_function(Y, X, lamb, betas):
     if np.sum(X[1]) == len(X[1]):
         return print('ERROR: X should not have an intercept')
     
-    loss = get_sse(Y, X, betas) + lamb * np.sum(np.abs(betas))
+    loss = sse(Y, X, betas) + lamb * np.sum(np.abs(betas))
     return loss
 
 
@@ -65,13 +66,64 @@ def beta_lasso(X, Y, lamb):
         which are not computationally efficient at O(p^3).
         SVD would be a more efficient approach.
         """
-    Z = preprocessing.scale(X_train)
+    Z = preprocessing.scale(X)
     Y_c = Y - np.mean(Y)
     
     left = np.linalg.inv(Z.transpose().dot(Z).values() + lamb * np.identity(X.shape[1]))
     right = Z.transpose().dot(Y_c)
     coefficients = left.dot(right)
     return(coefficients)
+
+
+def lasso_se_boot(X, Y, lamb, betas, n_iter = 100, progress_disable = False):
+        """
+        Estimate Lasso standard errors through bootstrapping
+
+        Parameters
+        ----------
+        Y : Nx1 Matrix
+        X : Matrix
+        lamb : Lambda value to use for L2
+        betas : Vector of estimated coefficients
+        n_ter : Integer of number of iterations for bootstrapping
+        progress_disable : Disable option for tqdm progress bar
+
+        Returns
+        -------
+        boot_se : vector of standard errors
+        """
+    
+    nobs = X.shape[0]
+    K = int(X[1] - 1)
+
+    beta_hat_boots = np.zeros((n_iter, K))
+
+    for b_iter in tqdm(range(0, n_iter), disable=progress_disable):
+        b_index = np.random.choice(range(0, nobs), nobs, replace = True)
+
+        b_beta_hat = beta_lasso(Y, X, lamb)
+
+        # Saving coefficient estimates
+        beta_hat_boots[b_iter] = b_beta_hat
+
+    # Estimated coefficients from bootstrapping
+    beta_hat_boots = pd.DataFrame(beta_hat_boots)
+    beta_hat_boots.index.name = 'boot_iter'
+    beta_hat_boots.columns = X.columns.values.tolist()
+
+    # Bootstrapped variance of coefficient estimates
+    beta_hat_boot_var = beta_hat_boots.var(axis=0)
+
+    # Bootstrapped SE of coefficient estimates
+    beta_hat_boot_SE = np.sqrt(beta_hat_boot_var)
+
+    # Bootstrapped t-stats for null that coefficient = 0
+    ## note that we use the coefficient estimates from the full sample 
+    ## but the variance from the bootstrapping procedure
+    beta_hat_boot_t = beta_lasso(Y, X) / beta_hat_boot_SE
+
+    # Bootstrapped p values from t test (two-sided)
+    beta_hat_boot_p = pd.Series(2 * (1- t.cdf(np.abs(beta_hat_boot_t), df = nobs - K)), beta_hat_boot_t.index)
 
 
 def pick_lowest_lambda(X, Y):
