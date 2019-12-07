63691b3803f6bb5f525f1dee19bcff03b0084177
Nadav Tadelis <nadav.tadelis@gmail.com>
2019-12-07 11:44:43 -0500

    added synth data and tested coeff estimates from OLS

--- Projects/project_2_packages/paddleboat/test/test_OLS.py
+++ Projects/project_2_packages/paddleboat/test/test_OLS.py
@@ -5,6 +5,8 @@ import numpy as np
 import transcripty as tpy
 import unittest
 from OLS import beta_ols
+import statsmodels.api as sm
+import pandas as pd
 
 
 
@@ -31,7 +33,34 @@ class TestHPM(unittest.TestCase):
         p_a = gamma_min + (1 - gamma_min)/(1 + gamma_1*np.exp(-gamma_2*a))
         self.assertAlmostEqual(p_a, self.hpm(a, 0.0))
 
-    
+    def test_simple_b_ols(self):
+        np.random.seed(60683) # testing different seed
+        n = 50000
+        ## DGP ##
+        means = [3, -1.5, 1.1, 2.3, -1, 3]
+        cov = [
+            [1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 1]]
+
+        ## Data Generation ##
+        X1, X2, X3, X4, Z1, Z2 = np.random.multivariate_normal(means, cov, n).T
+        epsilon = np.random.normal(0, 1, n)
+
+        # True model
+        Y = 1.5 + 2.5*X1 + 2*X2 + 3*X3 + 6*X4 + epsilon
+
+        independent_vars = pd.DataFrame({'X1' : X1, 'X2' : X2, 'X3' : X3, 'X4' : X4})
+        independent_vars = sm.add_constant(independent_vars)
+        dependent_vars = Y
+
+        # running model
+        coeff_estimates = beta_ols(dependent_vars, independent_vars)
+        self.assertIsNone(np.testing.assert_almost_equal([1.5, 2.5, 2, 3, 6], coeff_estimates, decimal=1))
+
 
 
 if __name__ == "__main__":
