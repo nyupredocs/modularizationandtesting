7748a2feab6695ab7180923e7eccafce0282e246
Nadav Tadelis <nadav.tadelis@gmail.com>
2019-12-07 14:11:33 -0500

    fix some suff, function read incorrrectly, need to fix. unclear why beta_lasso reads into lasso_fit

--- Projects/project_2_packages/paddleboat/LASSO/LASSO.py
+++ Projects/project_2_packages/paddleboat/LASSO/LASSO.py
@@ -1,111 +1,72 @@
 import pandas as pd
 import numpy as np
-import OLS as ols
 from sklearn import preprocessing
-import tqdm
-
-def sse(Y, X, betas):
-        '''
-        Get sum of square errors
-
-        Parameters
-        ----------
-        Y : Nx1 Matrix
-        X : Matrix
-        betas : Vector of estimated coefficients
-
-        Returns
-        -------
-        sse : Sum of square errors
-        '''
-   
-    e = betas.dot(X.values.T)-Y
-    sse = np.sum(e**2)
-    return sse
+from tqdm import tqdm
+from scipy.stats import t
 
 
-def loss(Y, X, lamb, betas):
-        """
-        Compute loss function
+def beta_lasso(Y, X, lamb):
+    """
+    Compute lasso coeffs
 
-        Parameters
-        ----------
-        Y : Nx1 Matrix
-        X : Matrix (no intercept column)
-        lamb : Lambda value to use for L2
-        betas : Vector of estimated coefficients
+    Parameters
+    ----------
+    Y : Nx1 Matrix
+    X : Matrix (with intercept column)
+    lamb : Lambda value to use for L2
 
-        Returns
-        -------
-        loss : Computed loss
-        """
-    if np.sum(X[1]) == len(X[1]):
-        return print('ERROR: X should not have an intercept')
+    Returns
+    -------
+    coefficients : Vector of Lasso coefficients
+
+    Note
+    ----
+    For simplicity we use matrix inverses,
+    which are not computationally efficient at O(p^3).
+    SVD would be a more efficient approach.
+    """
     
-    loss = sse(Y, X, betas) + lamb * np.sum(np.abs(betas))
-    return loss
-
-
-def beta_lasso(X, Y, lamb):
-        """
-        Compute lasso coeffs
-
-        Parameters
-        ----------
-        Y : Nx1 Matrix
-        X : Matrix (no intercept column)
-        lamb : Lambda value to use for L2
-
-        Returns
-        -------
-        coefficients : Vector of Lasso coefficients
-
-        Note
-        ----
-        For simplicity we use matrix inverses,
-        which are not computationally efficient at O(p^3).
-        SVD would be a more efficient approach.
-        """
-    Z = preprocessing.scale(X)
+    Z = X.iloc[:, 1:]
+    Z = pd.DataFrame(preprocessing.scale(Z))
     Y_c = Y - np.mean(Y)
     
-    left = np.linalg.inv(Z.transpose().dot(Z).values() + lamb * np.identity(X.shape[1]))
+    left = np.linalg.inv(Z.transpose().dot(Z) + lamb * np.identity(Z.shape[1]))
     right = Z.transpose().dot(Y_c)
     coefficients = left.dot(right)
     return(coefficients)
 
 
-def lasso_fit(X, Y, lamb, n_iter = 100, progress_disable = False):
-        """
-        Estimate Lasso standard errors through bootstrapping
 
-        Parameters
-        ----------
-        Y : Nx1 Matrix
-        X : Matrix
-        lamb : Lambda value to use for L2
-        n_ter : Integer of number of iterations for bootstrapping
-        progress_disable : Disable option for tqdm progress bar
+def lasso_fit(Y, X, lamb, n_iter=100, progress_disable = False):
+    """
+    Estimate Lasso standard errors through bootstrapping
 
-        Returns
-        -------
-        results : Results wrapper with lasso results
-                  coefficients = Lasso coefficients from full sample
-                  bootstrap_coeffs = Lasso coefficients from bootstrapping procedure
-                  bootstrap_coeffs_var = Coefficient variance from bootstrapping
-                  bootstrap_coeffs_SE = Coefficient standard errors from bootstrapping
-                  bootstrap_coeffs_t = T-stats (from bootstrapping SE)
-                  bootstrap_coeffs_p = P-values
-        """
-    
-    nobs = X.shape[0]
-    K = int(X[1] - 1)
+    Parameters
+    ----------
+    Y : Nx1 Matrix
+    X : Matrix
+    lamb : Lambda value to use for L2
+    n_ter : Integer of number of iterations for bootstrapping
+    progress_disable : Disable option for tqdm progress bar
+
+    Returns
+    -------
+    results : Results wrapper with lasso results
+                coefficients = Lasso coefficients from full sample
+                bootstrap_coeffs = Lasso coefficients from bootstrapping procedure
+                bootstrap_coeffs_var = Coefficient variance from bootstrapping
+                bootstrap_coeffs_SE = Coefficient standard errors from bootstrapping
+                bootstrap_coeffs_t = T-stats (from bootstrapping SE)
+                bootstrap_coeffs_p = P-values
+    """
+    nobs = np.shape(X)[0]
+    K = np.shape(X)[1] - 1
 
     beta_hat_boots = np.zeros((n_iter, K))
 
     for b_iter in tqdm(range(0, n_iter), disable=progress_disable):
         b_index = np.random.choice(range(0, nobs), nobs, replace = True)
-        Y, X= Y.iloc[b_index], X.iloc[b_index]
+        Y, X= pd.DataFrame(Y).iloc[b_index], pd.DataFrame(X).iloc[b_index]
 
         b_beta_hat = beta_lasso(Y, X, lamb)
 
@@ -140,3 +101,41 @@ def lasso_fit(X, Y, lamb, n_iter = 100, progress_disable = False):
                         bootstrap_coeffs_p = beta_hat_boot_p)
 
 
+def sse(Y, X, betas):
+    '''
+    Get sum of square errors
+
+    Parameters
+    ----------
+    Y : Nx1 Matrix
+    X : Matrix
+    betas : Vector of estimated coefficients
+
+    Returns
+    -------
+    sse : Sum of square errors
+    '''
+   
+    e = betas.dot(X.values.T)-Y
+    sse = np.sum(e**2)
+    return sse
+
+
+def loss(Y, X, lamb, betas):
+    """
+    Compute loss function
+
+    Parameters
+    ----------
+    Y : Nx1 Matrix
+    X : Matrix (with intercept column)
+    lamb : Lambda value to use for L2
+    betas : Vector of estimated coefficients
+
+    Returns
+    -------
+    loss : Computed loss
+    """
+    
+    loss = sse(Y, X, betas) + lamb * np.sum(np.abs(betas))
+    return loss
