bd3221f60e32288a2c15e4e1df2aabd793e50117
Ed Jee <edjee96@gmail.com>
2019-12-06 23:23:56 -0500

    standard errors no account for constant option

--- Projects/project_2_packages/iv_jett/iv_standard_error.py
+++ Projects/project_2_packages/iv_jett/iv_standard_error.py
@@ -17,7 +17,16 @@ def calculate_sigma(Z, X, Y, beta_iv, nocons = False):
     sigma_iv = (1/N) * np.transpose(resid) @ resid
     return np.sqrt(sigma_iv)
 
-def calculate_var_beta(sigma, X, Z):
+def calculate_var_beta(sigma, X, Z, nocons = False):
+    
+    N = Z.shape[0]
+
+    if nocons == False:
+        X_constant = np.ones((N, 1))
+        X = np.hstack((X_constant, X))
+        Z_constant = np.ones((N, 1))
+        Z = np.hstack((Z_constant, Z))
+
     var_beta = (np.asscalar(sigma**2) * np.identity(X.shape[1])) @ np.linalg.inv(np.transpose(X) @ projection_matrix(Z) @ X)
     se_beta = np.sqrt(np.diag(var_beta))
     return se_beta
