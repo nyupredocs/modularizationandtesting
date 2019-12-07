eed1eac4b52b8a854cfe2a56d64ba5f8fde92268
joel-becker <joel_becker@mail.com>
2019-12-07 11:49:59 -0500

    Very minor changes

--- Projects/project_2_packages/paddleboat/test/test_OLS.py
+++ Projects/project_2_packages/paddleboat/test/test_OLS.py
@@ -2,7 +2,6 @@ import sys
 sys.path.insert(0,'../OLS')
 
 import numpy as np
-import transcripty as tpy
 import unittest
 from OLS import beta_ols
 import statsmodels.api as sm
@@ -60,7 +59,6 @@ class TestHPM(unittest.TestCase):
         # running model
         coeff_estimates = beta_ols(dependent_vars, independent_vars)
         self.assertIsNone(np.testing.assert_almost_equal([1.5, 2.5, 2, 3, 6], coeff_estimates, decimal=1))
-
 
 
 if __name__ == "__main__":
