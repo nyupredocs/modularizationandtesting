f7fa641c94c8096c6ca219d592ab2b2bc421b52d
joel-becker <joel_becker@mail.com>
2019-12-08 10:25:58 -0500

    Pseuocode Jim's algo

---
+++ Projects/project_2_packages/paddleboat/simulation/morning.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.insert(0,'../OLS')
+
+import numpy as np
+import unittest
+from OLS import beta_ols, Sigma
+import statsmodels.api as sm
+import pandas as pd
+
+def simulation(X, Y):
+    beta_hat = norm(0,1)
+    sigma_hat = exp(norm(0,1))
+    y_hat = norm(X * beta_hat, sigma_hat)
+
+    fit = OLS.beta_ols(Y, X)
+    beta_model = OLS.betas(Y,X)
+    covariance_model = OLS.covariance(Y,X)
