5d7f9234907428c2a3f0f3675784ddd3099f471a
Gen <46057738+gen-li@users.noreply.github.com>
2019-12-08 08:59:24 -0500

    Update logit.py

--- Projects/project_2_packages/Team_cool/OLS_team_cool/logit.py
+++ Projects/project_2_packages/Team_cool/OLS_team_cool/logit.py
@@ -1,13 +1,38 @@
 import numpy as np
 import scipy.stats as st
-import randrange
-irand = randrange (0,1)
-x = np.random.randn(100,2)
-# eps = np.random.normal(0,1,(100,1))
-y = (np.array([5, 10]) @ x.T).reshape(100,1) + eps
-beta = np.random.randn(2,1)
-# cf = 0.95
--y.T -x @ beta
+import statsmodels as sm
+from scipy import optimize
+
+y = np.random.randint(2, size=(100,1))
+x = np.random.normal(0,1,(100,2))
+
+res_correct = sm.discrete.discrete_model.Logit(y,x).fit()
+res_correct.params
+
+
+def Logit(b,y,x):
+    # y = np.random.randint(2, size=(100,1))
+    # x = np.random.normal(0,1,(100,2))
+    n = x.shape[0]
+    # b = np.zeros((s,1))
+    # log_likelihood = (y.T @ x @ b)[0] - np.log(1 + np.exp(x.T @ b))
+    log_likelihood = -y.T @ np.log(1 + np.exp(-x @ b)) + (np.ones((n,1)) - y).T @ np.log(1 - 1 / (1 + np.exp(- x @ b)))
+
+    return -log_likelihood[0]
+
+Logit(y,x,np.array((2,1)))
+
+s = x.shape[1]
+b_0 = np.array((0,0))
+
+optimize.minimize(Logit,x0=b_0,args=(y,x))
+
+
+optimize.fmin_bfgs(Logit, b_0,args=(y,x,))
+
+y.shape
+
+
 # def OLS(y,x,cf=0.95):
 #     """
 #     OLS estimation.
